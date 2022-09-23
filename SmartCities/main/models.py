a354fe6a00c4635598f445459614f99c0dba18bd
Catalina Musalem lyng <catalina@MacBook-Pro-de-Catalina.local>
2022-09-23 12:53:12 -0300

    initialapp

---
+++ SmartCities/main/models.py
@@ -0,0 +1,24 @@
+from pickle import TRUE
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile = models.IntegerField(null=True)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+
+class Event(models.Model):
+    type = models.TextField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+    location = models.TextField()
+    message = models.TextField()
+    level = models.IntegerField()

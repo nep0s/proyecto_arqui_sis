2cd8accab576509f1399c011297a6ea86226255a
Catalina Musalem lyng <catalina@MacBook-Pro-de-Catalina.local>
2022-10-08 14:22:12 -0300

    Docker con workers

--- complejidad/complejidad/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import complejidad
-
-admin.site.register(complejidad)
-
--- complejidad/complejidad/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import complejidad
-
-class complejidadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = complejidad
-        fields = ('id', 'type', 'lat', 'lon', 'location', 'message', 'level')

037b2feaca9f732e4f3529c8a6d15aa72a2968e0
Sebastian Mitjans <smitjans@uc.cl>
2022-11-10 12:45:29 -0300

    token recibido correctamente

--- frontreact/src/Routing.js
+++ frontreact/src/Routing.js
@@ -1,16 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import EventScreen from './components/Events/EventScreen';
-import EventDetailsScreen from './components/Events/EventDetailsScreen';
 import LoginScreen from './components/Auth/LoginScreen';
 import SignupScreen from './components/Auth/SignupScreen.js';
+import EventScreen from './components/Events/EventScreen';
+import EventDetailsScreen from './components/Events/EventDetailsScreen';
 
 function Routing(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<EventScreen/>}/>
                 <Route path={"/login"} element={<LoginScreen/>}/>
                 <Route path={"/signup"} element={<SignupScreen/>}/>
+                <Route path={"/"} element={<EventScreen/>}/>
                 <Route path={"/:id"} element={<EventDetailsScreen/>}/>
             </Routes>
         </BrowserRouter>
